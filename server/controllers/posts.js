810402e24895ad58c8a8fcc70a7299867374f4b8
Valdir Silva <valdir.ti@gmail.com>
2022-03-28 22:16:56 -0300

    feat/add-async-to-create-post

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -13,7 +13,7 @@ export const getPost = async (req, res) => {
     }
 }
 
-export const createPost = (req, res) => {
+export const createPost = async (req, res) => {
     const post = req.body
     
     const newPost = new PostMessage(post)
