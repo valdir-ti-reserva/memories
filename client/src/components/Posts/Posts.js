c38e65510bc4dd049a04f97c19bfbc4582c7d44c
Valdir Silva <valdir.ti@gmail.com>
2022-03-29 14:12:18 -0300

    feat: apply defaull styles call on files

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,7 +2,10 @@ import React from "react"
 
 import Post from './Post/Post'
 
+import useStyles from './styles'
+
 const Posts = () => {
+    const classes = useStyles()
     return (
         <>
             <h1>Posts</h1>
