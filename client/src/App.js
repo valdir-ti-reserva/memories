fc2a47ab0f9f497ca8482a7376349321e39f2624
Valdir Silva <valdir.ti@gmail.com>
2022-04-02 00:49:13 -0300

    feat: config dispath redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
 
+import { getPosts } from './redux/actions/posts'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
 
@@ -11,6 +13,11 @@ import memories from './images/memories.png'
 const App = () => {
 
   const classes = useStyles()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [dispatch])
 
   return (
     <Container maxWidth="lg">
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,11 +1,17 @@
 import React from "react"
+import { useSelector } from 'react-redux'
 
 import Post from './Post/Post'
 
 import useStyles from './styles'
 
 const Posts = () => {
+    
     const classes = useStyles()
+    const posts = useSelector((state) => state.posts)
+
+    console.log('posts ===> ', posts)
+
     return (
         <>
             <h1>Posts</h1>
