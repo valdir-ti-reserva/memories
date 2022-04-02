21aaa5477e0091edc20069b749f4b3eb5a36c57e
Valdir Silva <valdir.ti@gmail.com>
2022-04-02 00:56:53 -0300

    feat: config enviroment variable

--- server/index.js
+++ server/index.js
@@ -1,9 +1,12 @@
+import 'dotenv/config'
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
 
 import postRoutes from './routes/posts.js'
+
+const { APP_PORT, APP_MONGO_USER, APP_MONGO_PASS } = process.env;
 
 const app = express()
 
@@ -14,15 +17,14 @@ app.use(cors())
 
 app.use('/posts', postRoutes)
 
-const CONNECTION_URL = "mongodb+srv://javascriptmastery:javascriptmastery123@cluster0.p1fnj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-const PORT = process.env.PORT || 5000
+const CONNECTION_URL = `mongodb+srv://${APP_MONGO_USER}:${APP_MONGO_PASS}@cluster0.p1fnj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 
 mongoose.connect(CONNECTION_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`)
+    app.listen(APP_PORT, () => {
+        console.log(`Server running on port ${APP_PORT}`)
     })
 }).catch((err) => {
     console.log(`Failed to database connect ${err}`)
